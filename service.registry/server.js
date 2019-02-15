e7da3467f5b615748f7dc6bfd39fa9bde48307aa
Ruben <rwoldhui@avans.nl>
2019-02-15 15:55:26 +0100

    [Added] http status codes

--- service.registry/server.js
+++ service.registry/server.js
@@ -10,35 +10,42 @@ const path = require('path');
 
 console.log(devices)
 console.log(rooms)
+
+//TODO: Handle errors & Send status
+//TODO: Verify body from request (check for duplicates and valid format)
+
+
+function createError(status, content) {
+    let error = {
+        "status": status,
+        "error": content
+    }
+    return error
+}
+
 // Root endpoint
 app.get('/', (req, res) => {
+    res.sendStatus(200)
     res.sendFile(path.join(__dirname + '/index.html'));
 })
 
 // Get all available devices
 app.get('/devices', (req, res) => {
+    res.sendStatus(200)
     res.send(devices)
 })
 
 // Add a new device
 app.post('/devices', (req, res) => {
-    //TODO: Verify body from request (look for duplicates and valid format)
     let json = req.body
     devices.push(json)
     fs.writeFileSync('./devices.json', JSON.stringify(devices), 'utf8')
+    res.sendStatus(201)
     res.send(devices)
 })
 
 // Get specific device details
-app.get('/device/:identifier', (req, res) => {
-    let id = req.params['identifier']
-    console.log(id)
-    res.send(id)
-    //TODO: Load from file & send response
-})
-
-// Delete a specific device
-app.delete('/device/:identifier', (req, res) => {
+app.get('/devices/:identifier', (req, res) => {
     let id
     try {
         id = req.params['identifier']
@@ -48,15 +55,41 @@ app.delete('/device/:identifier', (req, res) => {
     }
     for (let i = 0; i < devices.length; i++) {
         if (devices[i].identifier === id) {
-            devices.splice(i, 1)
+            res.sendStatus(200)
+            res.send(devices[i])
+            return
         }
     }
-    fs.writeFileSync('devices.json', JSON.stringify(devices), 'utf8')
-    res.send(devices)
+    res.sendStatus(404)
+})
+
+// Delete a specific device
+app.delete('/devices/:identifier', (req, res) => {
+    let id
+    let found = false
+    try {
+        id = req.params['identifier']
+    } catch (err) {
+        console.log('In catch')
+        console.error(err)
+    }
+    for (let i = 0; i < devices.length; i++) {
+        if (devices[i].identifier === id) {
+            devices.splice(i, 1)
+            found = true
+        }
+    }
+    if (found) {
+        fs.writeFileSync('devices.json', JSON.stringify(devices), 'utf8')
+        res.sendStatus(204)
+    } else {
+        res.sendStatus(404)
+    }
 })
 
 // Get all available rooms
 app.get('/rooms', (req, res) => {
+    res.sendStatus(200)
     res.send(rooms)
 })
 
@@ -65,24 +98,51 @@ app.post('/rooms', (req, res) => {
     let json = req.body
     rooms.push(json)
     fs.writeFileSync('rooms.json', JSON.stringify(rooms), 'utf8')
+    res.sendStatus(201)
     res.send(rooms)
-    //TODO: Append to file & send response
 })
 
 // Get specific room details
-app.get('/room/:identifier', (req, res) => {
-    let id = req.params['identifier']
-    console.log(id)
-    res.send(id)
-    //TODO: Load from file & send response
+app.get('/rooms/:identifier', (req, res) => {
+    let id
+    try {
+        id = req.params['identifier']
+    } catch (err) {
+        console.log('In catch')
+        console.error(err)
+    }
+    for (let i = 0; i < rooms.length; i++) {
+        if (rooms[i].identifier === id) {
+            res.sendStatus(200)
+            res.send(rooms[i])
+            return
+        }
+    }
+    res.sendStatus(404)
 })
 
 // Delete a specific room
-app.delete('/room/:identifier', (req, res) => {
-    let id = req.params['identifier']
-    console.log(id)
-    res.send(id)
-    //TODO: Delete from file & send response
+app.delete('/rooms/:identifier', (req, res) => {
+    let id
+    let found = false
+    try {
+        id = req.params['identifier']
+    } catch (err) {
+        console.log('In catch')
+        console.error(err)
+    }
+    for (let i = 0; i < rooms.length; i++) {
+        if (rooms[i].identifier === id) {
+            rooms.splice(i, 1)
+            found = true
+        }
+    }
+    if (found) {
+        fs.writeFileSync('rooms.json', JSON.stringify(rooms), 'utf8')
+        res.sendStatus(204)
+    } else {
+        res.sendStatus(404)
+    }
 })
 
 app.listen(80, () => {
