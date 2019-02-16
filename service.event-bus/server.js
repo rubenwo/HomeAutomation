4114bc904f7f8cdbbdf3c7da9179ab9db9e73871
Ruben <rwoldhui@avans.nl>
2019-02-16 14:18:53 +0100

    [Added] event service package
    Clients can subscribe to events. Controllers will post events to this service. Service then redistributes it to all subscribed clients

---
+++ service.event-bus/server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const WebSocket = require('ws')
+const http = require('http')
+
+const app = express()
+const server = http.createServer(app)
+const wss = new WebSocket.Server({ server })
+
