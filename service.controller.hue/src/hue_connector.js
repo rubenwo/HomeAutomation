37c290680c5aed9ce729b730812aa04aaac7bfd6
Ralph <rasrouwe@avans.nl>
2019-03-11 12:29:56 +0100

    [Added] hue methods

--- service.controller.hue/src/hue_connector.js
+++ service.controller.hue/src/hue_connector.js
@@ -1,18 +1,6 @@
 const request = require('request')
 
 class Connector {
-    pair(url, callback) {
-
-        
-        let body = {
-            "devicetype": "NodeJS-Controller"
-        }
-
-        this.sendRequest(url, "POST", body, (err, resp, body) => {
-            if (body[0].success)
-                callback(body[0].success.username)
-        })
-    }
 
     sendRequest(url, method, body, callback) {
         console.log("Sending request")
