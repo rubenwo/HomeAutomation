ed49ed51a4c190bba7dd5ff64d4bbed41163b25a
Ruben <rwoldhui@avans.nl>
2019-03-16 21:33:09 +0100

    [Added] Dashboard layout for client.web

--- client.web/src/App.vue
+++ client.web/src/App.vue
@@ -1,28 +1,7 @@
 <template>
-  <div id="app">
-    <Header/>
-    <router-view/>
-  </div>
+  <router-view></router-view>
 </template>
 
 <script>
-import Header from "./components/layout/Header";
-export default {
-  name: "app",
-  components: {
-    Header
-  }
-};
+export default {};
 </script>
-
-<style>
-* {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-}
-body {
-  font-family: Arial, Helvetica, sans-serif;
-  line-height: 1.4;
-}
-</style>
---
+++ client.web/src/pages/Devices.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="content"></div>
+</template>
+
+<script>
+import { StatsCard } from "@/components";
+
+export default {
+  components: {
+    StatsCard
+  },
+  data() {
+    return {
+      responsiveOptions: [
+        [
+          "screen and (max-width: 640px)",
+          {
+            seriesBarDistance: 5,
+            axisX: {
+              labelInterpolationFnc: function(value) {
+                return value[0];
+              }
+            }
+          }
+        ]
+      ]
+    };
+  }
+};
+</script>
