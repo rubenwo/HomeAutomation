f8764fdaff84957e60256e8de3e212ee73c8eb0f
BartAvans <b.vanes1@student.avans.nl>
2019-03-18 15:40:55 +0100

    [Added] Something

--- client.web/src/api/ApiClient.js
+++ client.web/src/api/ApiClient.js
@@ -18,4 +18,51 @@ export default class ApiClient {
         let response = await axios.get(this.apiGatewayUrl + "/registry/rooms")
         return response.data.rooms;
     }
+
+    async getUser(){
+        let response = await axios.get(this.apiGatewayUrl + "/hue/pair")
+        return response.data.user;
+    }
+
+    async fetchLamps(){
+        let response = await axios.get(this.apiGatewayUrl + "/hue/lamps");
+        return response.data.lamps;
+    }
+
+    async fetchLampId(){
+        let response = await axios.get(this.apiGatewayUrl + "/hue/lamp/:identifier");
+        return response.data.lamp;
+    }
+    
+    async changeLampId(id){
+        let response = await axios.put(this.apiGatewayUrl + "/hue/lamp/:identifier")
+        return response.data.lamp.id;
+    }
+
+    async changeStateLamp(state){
+        let response = await axios.put(this.apiGatewayUrl + "/hue/lamp/:identifier")
+        return response.data.lamp.state;
+    }
+
+    async changeIrName(name){
+        let response = await axios.put(this.apiGatewayUrl + "/ir_controller/encoding_tables")
+        return response.data.ir.name;
+    }
+
+    async fetchIrName(){
+        let response = await axios.get(this.apiGatewayUrl + "/ir_controller/encoding_tables")
+        return response.data.ir;
+    }
+
+    async changeIrCommand(command){
+        let response = await axios.put(this.apiGatewayUrl + "/ir_controller/encoding_tables")
+        return response.data.ir.command;
+    }
+
+    async fetchIrCommand(){
+        let response = await axios.get(this.apiGatewayUrl + "/ir_controller/encoding_tables")
+        return response.data.ir.command;
+    }
+
+    //TODO: HUE & IR
 }
