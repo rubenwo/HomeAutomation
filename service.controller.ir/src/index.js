2d1d3201076228a6cd44f4d498dd24150db4d1e7
Ruben <rwoldhui@avans.nl>
2019-03-26 21:52:53 +0100

    [Fixed] express in ir service

--- service.controller.ir/src/index.js
+++ service.controller.ir/src/index.js
@@ -1,51 +1,37 @@
 const express = require('express')
-const cors = require('cors')
-const app = express()
 const delay = require('delay')
+const app = express()
+const cors = require('cors')
 
-const gpio = require('rpi-gpio')
-const gpiop = gpio.promise;
-
-const ledPin = 7
-
-gpiop.setup(ledPin, gpio.DIR_OUT)
-    .then(() => {
-        return gpiop.write(ledPin, true)
-    })
-    .catch((err) => {
-        console.log('Error: ', err.toString())
-    })
-
-
-app.use(cors)
+app.use(cors())
+app.use(express.json())
 
 app.get("/ir_controller/encoding_tables", (req, res) => {
-    res.sendStatus(500)
+    console.log("In /ir_controller/encoding_tables")
+    res.sendStatus(200)
 })
 
 app.post("/ir_controller/command", (req, res) => {
-    console.log("Got a request in /command")
+    console.log("In /ir_controller/command")
     let data = req.body
+    console.log(data)
     let json = {
         "command": data["command"],
-        "device": data["device"]
+        "devuce": data["device"]
     }
-        blink()
+    blink()
     res.sendStatus(200)
 })
 
 async function blink() {
     for (let i = 0; i < 5; i++) {
-        gpio.write(ledPin, true)
-        console.log('on')
-        await delay(500)
-        gpio.write(ledPin, false)
-        console.log('off')
-        await delay(500)
+        console.log("on")
+        await delay(100)
+        console.log("off")
+        await delay(100)
     }
 }
 
 app.listen(80, () => {
-    blink()
-    console.log('IR Controller service started listening on port 80...')
+    console.log("IR Controller Service is listening on port 80...")
 })
