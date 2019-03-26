4ec2fdcf11f81255f2493a468f21e34140f1e7eb
Ruben <rwoldhui@avans.nl>
2019-03-26 22:01:59 +0100

    [Added] onoff lib to ir-controller

--- service.controller.ir/src/index.js
+++ service.controller.ir/src/index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const delay = require('delay')
 const app = express()
 const cors = require('cors')
+const Gpio = require('onoff').Gpio
+
+const LED = new Gpio(4, 'out')
+
 
 app.use(cors())
 app.use(express.json())
@@ -26,12 +30,15 @@ app.post("/ir_controller/command", (req, res) => {
 async function blink() {
     for (let i = 0; i < 5; i++) {
         console.log("on")
+        LED.writeSync(1)
         await delay(100)
         console.log("off")
+        LED.writeSync(0)
         await delay(100)
     }
 }
 
 app.listen(80, () => {
+    blink()
     console.log("IR Controller Service is listening on port 80...")
 })
